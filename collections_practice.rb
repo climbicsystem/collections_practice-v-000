3594c47299377bfe7a2d74711f95abe94e3254f2
Joseph Vint <josephvint@me.com>
2018-02-14 23:41:36 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -9,3 +9,11 @@ end
 def sort_array_char_count(strings)
   strings.sort {|a,b| a.length <=> b.length}
 end
+
+def swap_elements(array)
+  array.sort do |a,b|
+    if a == 1
+      b <=> a
+    end
+  end
+end
